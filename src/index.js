599a3b8835af97dd5bc487d5c9264edc6b5ead0b
Shaun Putnam <shaun.putnam@student.swic.edu>
2025-03-04 22:49:46 -0600

    Add lesson on callback functions

--- src/index.js
+++ src/index.js
@@ -18,3 +18,34 @@ const calcOrderCost = (order) => {
     return order.quantity * order.priceEa
 }
 
+function greet(name) {
+    return 'Hello, ${name}!'
+}
+
+console.log(greet('John'))
+
+function double(num) {
+    return num * 2
+}
+
+console.log(double(3))
+
+function doSomethingWithNumber(number, callbackFunction) {
+    return callbackFunction(number)
+}
+
+function divideByTwo(num) {
+    return num / 2
+}
+
+function double(num) {
+    return num * 2
+}
+
+function triple(num) {
+    return num * 3
+}
+
+doSomethingWithNumber(42, double)
+doSomethingWithNumber(42, triple)
+doSomethingWithNumber(42, divideByTwo)
