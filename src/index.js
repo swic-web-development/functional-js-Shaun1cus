e0ce8df749e37722e149b80bed25d7ef30577a47
Shaun Putnam <shaun.putnam@student.swic.edu>
2025-03-04 21:45:03 -0600

    Add lesson on arrow functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-var x = 3;
+const x = 3;
 
 console.log(x);
+
+const greet = functions () {
+    return 'Hello, world!'
+}
+
+const greet = () =? {
+    return 'Hello, world!'
+}
+
+function calculateOrderCost(order) {
+    return order.quantity * order.priceEa
+}
+
+const calcOrderCost = (order) => {
+    return order.quantity * order.priceEa
+}
+
